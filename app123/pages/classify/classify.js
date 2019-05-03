f78743b79a8f7b7fc0ef7ac638c77382f5b50286
yuzhilin4575 <1297278412@qq.com>
2019-05-03 21:26:57 +0800

    4

--- app123/pages/classify/classify.js
+++ app123/pages/classify/classify.js
@@ -1,66 +1,44 @@
-// pages/classify/classify.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-   
+    navLeftItems: [],
+    navRightItems: [],
+    curNav: 1,
+    curIndex: 0
   },
+  onLoad: function () {
+    // 加载的使用进行网络访问，把需要的数据设置到data数据对象
+    var that = this
+    wx.request({
+      url: 'http://rap2api.taobao.org/app/mock/167390/index/goods',
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+        if(res.data.code===0){
+          console.log(res.data.data)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+          that.setData({
+            navLeftItems: res.data.data,
+            navRightItems: res.data.data
+          })
+        }
+        
+       
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+ 
+  //事件处理函数
+  switchRightTab: function (e) {
+    // 获取item项的id，和数组的下标值
+    let id = e.target.dataset.id,
+      index = parseInt(e.target.dataset.index);
+    // 把点击到的某一项，设为当前index
+    this.setData({
+      curNav: id,
+      curIndex: index
+    })
   }
+
 })
+
