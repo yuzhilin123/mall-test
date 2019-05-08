0531db8cda66ec4858618d3c1eae21785d749034
yuzhilin <18828976@qq.com>
2019-05-09 04:16:44 +0800

    5570

--- app123/pages/index/movie-detail/movie-detail.js
+++ app123/pages/index/movie-detail/movie-detail.js
@@ -10,7 +10,7 @@ Page({
     likeHidden: true,
     unlikeHidden: false,
     animationInfo: {},
-    animationOpacity: 1
+    animationOpacity: 0
   },
   goShouye: function(event) {
     wx.switchTab({
@@ -68,7 +68,59 @@ Page({
     });
   },
 
+  // 添加商品到购物车
+  addToCart() {
+   var me=this;
+    
+    me.setData({
+      animationOpacity: 1
+    })
 
+    me.showAddToCartAnimation();
+
+
+    // 商品id存入缓存购物车
+    // var itemId = me.data.item.id;
+    // me.cartItemIncrease(itemId);
+  },
+  // 实现动画效果
+  showAddToCartAnimation() {
+    // 创建动画
+    var animation = wx.createAnimation({
+      duration: 500
+    });
+    this.animation = animation;
+
+    // rotate: 旋转
+    // this.animation.rotate(-180).step();
+    // translateX 在水平轴上移动
+    // this.animation.translateX("296rpx").step();
+    // 旋转的同时，又在水平轴上移动
+    this.animation.rotate(-180).translateX("80px").step();
+
+    this.setData({
+      // 导出动画效果到页面
+      animationInfo: this.animation.export()
+    });
+
+    // 复原动画
+    setTimeout(function () {
+      this.setData({
+        animationOpacity: 0,
+        cartIco: "cart-full",
+      });
+
+      setTimeout(function () {
+        this.animation.rotate(0).translateX(0).step({
+          duration: 0
+        });
+        this.setData({
+          animationInfo: this.animation.export()
+        });
+      }.bind(this), 550);
+    }.bind(this), 600);
+
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
