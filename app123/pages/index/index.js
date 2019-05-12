32d75cfe25e683e6ff5be8cb13d2883fd65f5ffc
yuzhilin <18828976@qq.com>
2019-05-12 14:13:45 +0800

    1Function Key 9

--- app123/pages/index/index.js
+++ app123/pages/index/index.js
@@ -98,7 +98,7 @@ Page({
     //创建动画实例
     var animation = wx.createAnimation({
       //此处以公告最长内容来设置动画持续时间（duration：决定整个动画播放的速度）
-      duration: 25000,
+      duration: 20000,
       timingFunction: 'linear'
     });
     //偏移距离为公告内容的长度*字体大小（若字体大小使用rpx需要换算成px）
@@ -109,18 +109,18 @@ Page({
     // 循环播放动画关键步骤（使用两个定时器）
     // 第一个定时器：将字幕恢复到字幕开始点（为屏幕左边）
     this.recoveraAnimation = setInterval(function () {
-      animation.translate(295, 0).step({ duration: 0 });
+      animation.translate(300, 0).step({ duration: 0 });
       this.setData({
         animationData: animation.export()
       });
-    }.bind(this), 26000);
+    }.bind(this), 20000);
     // 第二个定时器：重新开始移动动画
     this.restartAnimation = setInterval(function () {
       animation.translate(-Number(this.data.announcementText.length * 16), 0).step();
       this.setData({
         animationData: animation.export()
       });
-    }.bind(this), 27000);
+    }.bind(this), 21000);
   
 
   
