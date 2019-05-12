430418d42f1f5e6b8457199fc0c22ba278b0cdfd
yuzhilin <18828976@qq.com>
2019-05-13 03:37:55 +0800

    337

--- app123/pages/index/movie-detail/movie-detail.js
+++ app123/pages/index/movie-detail/movie-detail.js
@@ -14,7 +14,11 @@ Page({
     animationOpacity: 0,
     showShopPopup:false,
     showSuccess:false,
-    animationData: {} // 动画数据
+    animationData: {}, // 动画数据
+    // input默认是1
+    num: 1,
+    // 使用data数据对象设置样式名
+    minusStatus: 'disabled'
   },
   goShouye: function(event) {
     wx.switchTab({
@@ -205,7 +209,42 @@ Page({
   },
   /**
    * 生命周期函数--监听页面隐藏
-   */
+   */    bindMinus: function () {
+    var num = this.data.num;
+    // 如果大于1时，才可以减
+    if (num > 1) {
+      num--;
+    }
+    // 只有大于一件的时候，才能normal状态，否则disable状态
+    var minusStatus = num <= 1 ? 'disabled' : 'normal';
+    // 将数值与状态写回
+    this.setData({
+      num: num,
+      minusStatus: minusStatus
+    });
+  },
+  /* 点击加号 */
+  bindPlus: function () {
+    var num = this.data.num;
+    // 不作过多考虑自增1
+    num++;
+    // 只有大于一件的时候，才能normal状态，否则disable状态
+    var minusStatus = num < 1 ? 'disabled' : 'normal';
+    // 将数值与状态写回
+    this.setData({
+      num: num,
+      minusStatus: minusStatus
+    });
+  },
+  /* 输入框事件 */
+  bindManual: function (e) {
+    var num = e.detail.value;
+    // 将数值与状态写回
+    this.setData({
+      num: num
+    });
+  },
+
   onHide: function() {
 
   },
